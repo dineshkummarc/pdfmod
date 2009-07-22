9f2cf47957321bff069f5021730ae032b651bdc3
Gabriel Burt <gabriel.burt@gmail.com>
2009-07-21 18:00:08 -0700

    Add COPYING, MAINTAINERS, and Document/Page.cs

---
+++ src/PdfMod/PdfMod/Document.cs
@@ -0,0 +1,222 @@
+
+using System;
+using System.Collections.Generic;
+
+using Hyena;
+
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace PdfMod
+{
+    public class Document : IDisposable
+    {
+        private PdfDocument pdf_document;
+        private List<Page> pages = new List<Page> ();
+        private string password;
+        private string tmp_uri;
+
+        public string Uri { get; private set; }
+        public string Path { get; private set; }
+        public PdfDocument Pdf { get { return pdf_document; } }
+        public int Count { get { return pages.Count; } }
+
+        public IEnumerable<Page> Pages {
+            get {
+                foreach (var page in pages)
+                    yield return page;
+            }
+        }
+
+        public Page this [int index] {
+            get { return pages[index]; }
+        }
+
+        public event Action<int, Page[]> PagesMoved;
+        public event Action<Page []> PagesRemoved;
+        public event Action<int, Page []> PagesAdded;
+        public event Action<Page []> PagesChanged;
+
+        public Document (string uri, string password)
+        {
+            Uri = new Uri (uri).AbsoluteUri;
+            Path = new Uri (uri).AbsolutePath;
+            this.password = password;
+
+            pdf_document = PdfSharp.Pdf.IO.PdfReader.Open (Path, password, PdfDocumentOpenMode.Modify);
+            for (int i = 0; i < pdf_document.PageCount; i++) {
+                var page = new Page (pdf_document.Pages[i]) {
+                    Document = this,
+                    Index = i
+                };
+                pages.Add (page);
+            }
+
+            UpdateThumbnails (pages);
+        }
+
+        private const int SIZE = 256;
+        private void UpdateThumbnails (IEnumerable<Page> update_pages)
+        {
+            Console.WriteLine ("Trying to load thumbs for {0}", tmp_uri ?? Uri);
+            using (var doc = Poppler.Document.NewFromFile (tmp_uri ?? Uri, password ?? "")) {
+                foreach (var page in update_pages) {
+                    using (var pop_page = doc.GetPage (IndexOf (page))) {
+                        // TODO try to use/get the embedded thumbnail?
+                        double w, h;
+                        pop_page.GetSize (out w, out h);
+                        double scale = SIZE / Math.Max (w, h);
+        
+                        int thumb_w = (int) Math.Ceiling (w * scale);
+                        int thumb_h = (int) Math.Ceiling (h * scale);
+                        page.Pixbuf = new Gdk.Pixbuf (Gdk.Colorspace.Rgb, false, 8, thumb_w, thumb_h);
+                        pop_page.RenderToPixbuf (0, 0, (int)w, (int)h, scale, 0, page.Pixbuf);
+                    }
+                }
+            }
+        }
+
+        private void Reindex ()
+        {
+            for (int i = 0; i < pages.Count; i++) {
+                pages[i].Index = i;
+            }
+        }
+
+        public void Dispose ()
+        {
+            if (pdf_document != null) {
+                pdf_document.Dispose ();
+                pdf_document = null;
+            }
+
+            if (tmp_uri != null) {
+                System.IO.File.Delete (tmp_uri);
+                tmp_uri = null;
+            }
+        }
+
+        public IEnumerable<Page> FindPagesMatching (string text)
+        {
+            using (var doc = Poppler.Document.NewFromFile (tmp_uri ?? Uri, password ?? "")) {
+                for (int i = 0; i < doc.NPages; i++) {
+                    using (var page = doc.GetPage (i)) {
+                        var list = page.FindText (text);
+                        if (list != null) {
+                            yield return pages[i];
+                            list.Dispose ();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Move (int to_index, Page [] move_pages)
+        {
+            for (int i = 0, new_index = to_index; i < move_pages.Length; i++, new_index++) {
+                var page = move_pages[i];
+                int old_index = pages.IndexOf (page);
+
+                // Move it in our list of Pages
+                pages.Remove (page);
+                pages.Insert (new_index, page);
+
+                // Move it in the actual document
+                pdf_document.Pages.MovePage (old_index, new_index);
+            }
+
+            Reindex ();
+            SaveTemp ();
+
+            var handler = PagesMoved;
+            if (handler != null) {
+                handler (to_index, move_pages);
+            }
+        }
+
+        public int IndexOf (Page page)
+        {
+            return pages.IndexOf (page);
+        }
+
+        public void Remove (params Page [] remove_pages)
+        {
+            foreach (var page in remove_pages) {
+                pdf_document.Pages.RemoveAt (pages.IndexOf (page));
+                pages.Remove (page);
+            }
+
+            Reindex ();
+            SaveTemp ();
+
+            var handler = PagesRemoved;
+            if (handler != null) {
+                handler (remove_pages);
+            }
+        }
+
+        public void Save (string uri)
+        {
+            Pdf.Save (uri);
+            Uri = uri;
+
+            if (tmp_uri != null) {
+                try {
+                    System.IO.File.Delete (tmp_uri);
+                } catch (Exception e) {
+                    Log.Exception ("Couldn't delete tmp file after saving", e);
+                } finally {
+                    tmp_uri = null;
+                }
+            }
+        }
+
+        private void SaveTemp ()
+        {
+            try {
+                if (tmp_uri == null) {
+                    tmp_uri = System.IO.Path.Combine (PdfMod.CacheDir, System.IO.Path.GetFileNameWithoutExtension (Uri) + ".pdf.tmp");
+                    if (System.IO.File.Exists (tmp_uri)) {
+                        System.IO.File.Delete (tmp_uri);
+                    }
+                }
+
+                pdf_document.Save (tmp_uri);
+            } catch (Exception e) {
+                Log.Exception ("Failed to save tmp document", e);
+            }
+        }
+
+        private void OnChanged (Page [] changed_pages)
+        {
+            // Update thumbnails
+            Reindex ();
+            UpdateThumbnails (changed_pages);
+
+            var handler = PagesChanged;
+            if (handler != null) {
+                handler (changed_pages);
+            }
+        }
+
+        public void Add (int to_index, params Page [] add_pages)
+        {
+            int i = to_index;
+            foreach (var page in add_pages) {
+                page.Document = this;
+                pages.Insert (i, page);
+                pdf_document.Pages.Insert (i, page.Pdf);
+                i++;
+            }
+
+            Reindex ();
+            SaveTemp ();
+            UpdateThumbnails (add_pages);
+
+            var handler = PagesAdded;
+            if (handler != null) {
+                handler (to_index, add_pages);
+            }
+        }
+    }
+}
---
+++ src/PdfMod/PdfMod/Page.cs
@@ -0,0 +1,23 @@
+
+using System;
+
+using PdfSharp.Pdf;
+
+namespace PdfMod
+{
+    public class Page
+    {
+        private PdfPage pdf_page;
+
+        internal PdfPage Pdf { get { return pdf_page; } }
+
+        public Document Document { get; internal set; }
+        public int Index { get; internal set; }
+        public Gdk.Pixbuf Pixbuf { get; internal set; }
+        
+        public Page (PdfPage pdf_page)
+        {
+            this.pdf_page = pdf_page;
+        }
+    }
+}
