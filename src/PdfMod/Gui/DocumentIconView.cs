a29cfe7f8f31ce003fa3e307d54d86a2a8e8cf19
Nuno Araujo <nuno.araujo@russo79.com>
2011-03-21 12:10:52 +0800

    Fix compilation under Mono 2.10 (bgo#644516)
    Force use of the overloaded implicit operator of Hyena.Gui.DragDropList
    to solve mono 2.10 compilation problems.
    
    Signed-off-by: Alexander Kojevnikov <alexander@kojevnikov.com>

--- src/PdfMod/Gui/DocumentIconView.cs
+++ src/PdfMod/Gui/DocumentIconView.cs
@@ -319,7 +319,7 @@ namespace PdfMod.Gui
                 if (to_index < 0)
                     return;
 
-                var pages = args.SelectionData.Data as Hyena.Gui.DragDropList<Page>;
+                Hyena.Gui.DragDropList<Page> pages = args.SelectionData;
                 to_index -= pages.Count (p => p.Index < to_index);
                 var action = new MoveAction (document, pages, to_index);
                 action.Do ();
